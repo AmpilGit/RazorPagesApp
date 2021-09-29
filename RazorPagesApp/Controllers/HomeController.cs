345212d80787d10a96975c35f10c933bb55dc110
AmpilGit <82020458+AmpilGit@users.noreply.github.com>
2021-09-29 07:03:14 +0300

    Добавьте файлы проекта.

---
+++ RazorPagesApp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorPagesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ApplicationContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.People.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                 Person file = new Person { Name = uploadedFile.FileName, PhotoPath = path };
+                _context.People.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RazorPagesApp/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Dostig { get; set; }
+        public string Surname { get; set; }
+        public string Work { get; set; }
+        public string Salary { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
